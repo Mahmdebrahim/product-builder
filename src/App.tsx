649e2192c5d36d8f25ac7472efd879ede8ad3b8b
Mahmdebrahim <muhammedebrahim712.com>
2025-08-12 21:57:56 +0300

    custom modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { productList } from "./components/Data"
 import ProductCard from "./components/ProductCard"
-
+import Button from "./components/Ui/Button"
+import Modal from "./components/Ui/Model"
+import { useState } from "react"
 const App = () => {
+   /* ________render_________ */
+   const products = productList.map(ptoduct => <ProductCard key={ptoduct.id}
+    product={ptoduct}/>);
+   ///////////////////////////////////////////////////////
+   /* ________Modal State_________ */   
+   const [isOpen, setIsOpen] = useState(false);
+
+   /* ________Modal Handeler_________ */ 
+   function open() {
+      setIsOpen(true)
+   }
+   
+   function close() {
+      setIsOpen(false)
+   }
+   ///////////////////////////////////////////////////////////////////////////
+   //! START POINT
     return (
        <main className="container mx-auto">
+          <Button className="bg-indigo-800" onClick={open}>build product</Button>
+          <Modal close={close} isOpen={isOpen} title="ADD A NEW PRODUCT" >
+           <div className="flex flex-co gap-2 items-center">
+               <Button className="bg-indigo-800" onClick={()=>{}}>Submit</Button>
+               <Button className="bg-gray-400 hover:bg-gray-500 "  onClick={close}>Cansle</Button>
+           </div>
+          </Modal>
           <div className=" p-2 m-5 grid grid-cols-1  sm:grid-cols-2       md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+           {products}
           </div>
        </main>
     )
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,20 +1,19 @@
 // import { Children } from "react";
+import type { IProduct } from "./Interfaces";
 import Button from "./Ui/Button";
+import { txtSlicer } from "./Utilities/txtSlicer";
+import Image from "./Image";
 interface IProps {
-
+    product: IProduct,
 }
-const ProductCard = ({}:IProps) => {
-
-
+const ProductCard = ({product}:IProps) => {
+    const {category, description, imageURL, price, title, } = product
     return (
-        <div className="border border-gray-200 p-2 rounded-lg flex flex-col gap-2 cursor-pointre max-w-sm m-auto sm:m-0">
-            <img 
-            src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvc2V8ZW58MHx8MHx8fDA%3D" 
-            alt="" 
-            className="rounded-lg md:max-w-md"/>
+        <div className="border border-gray-200 p-2 rounded-lg flex flex-col gap-2 cursor-pointre max-w-sm m-auto sm:m-0 ">
+            <Image ImgUrl={imageURL} alt={category.name} ClassName="rounded-lg md:max-w-md lg:object-cover h-52 w-full"/>
             <div className="">
-                <h3 className="font-bold">Nike athletic shoe</h3>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit Tenetur.</p>
+                <h3 className="font-bold">{title}</h3>
+                <p className="text-gray-500">{txtSlicer(description)}</p>
             </div>
             <div className="flex items-center gap-1 my-2">
                 <span className=" rounded-4xl bg-red-600 w-5 h-5 cursor-pointer"></span>
@@ -23,10 +22,10 @@ const ProductCard = ({}:IProps) => {
                 <span className=" rounded-4xl bg-red-950 w-5 h-5 cursor-pointer"></span>
             </div>
             <div className="flex items-center justify-between">
-                <span>$1750</span>
+                <span>${price}</span>
                 <div className="flex items-center gap-2">
-                    <span className="font-bold text-xs">Categor</span>
-                    <img src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvc2V8ZW58MHx8MHx8fDA%3D" alt="" className="h-10 w-10 rounded-full"/>
+                    <span className="font-bold text-xs">{category.name}</span>
+                    <img src={category.imageURL} alt={category.name} className="h-10 w-10 rounded-full"/>
                 </div>
             </div>
             <div className="w-full bg-blue-80 flex gap-2">
